b19855a06707a9c0d88c9f3777b82e1575123cb2
aleybe <alexbennettece@gmail.com>
2020-08-05 09:25:57 +0800

    updated with local time instead -.-

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,44 @@
 import { Component } from '@angular/core';
 import * as axios from 'axios';
 
+
+function fixDateTime (inputObj)
+{
+  let inputString = inputObj.name;
+
+  let inputHours = inputString.split(":")[0];
+  let inputMinutes = inputString.split(":")[1].split(" ")[0];
+  let inputPastNoon = inputString.split(":")[2].split(" ")[1];
+
+  let hours = inputHours;
+  let minutes = inputMinutes;
+
+  if(inputPastNoon == "PM")
+  {
+    hours = inputHours + 12;
+  }
+
+  if(inputPastNoon == "AM" && inputHours == 12)
+  {
+    hours = 0;
+  }
+
+  var DateObject = new Date();
+  var utcDate = DateObject.getUTCFullYear();
+
+  var year = DateObject.getUTCFullYear();
+  var month = DateObject.getUTCMonth();
+  var day = DateObject.getUTCDay();
+
+  var returnDate = new Date(year, month, day, hours, minutes, 0)
+  returnDate.setMinutes(returnDate.getMinutes() + 480);
+
+  //return returnDate.toTimeString() //{ name: returnDate.toTimeString() , value: inputObj.value };
+  return { name: returnDate.toTimeString().split(" ")[0] , value: inputObj.value };
+}
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,7 +59,12 @@ export class AppComponent {
         console.log(data);
         console.log(data.data.returnData[0]);
 
-        this.tempData = [{ name: 'Temperature', series: data.data.returnData[0].series }];
+        let cleanerDataset = data.data.returnData[0].series;
+        var results = cleanerDataset.map(fixDateTime)
+        console.log("res", results)
+        console.log(cleanerDataset);
+
+        this.tempData = [{ name: 'Temperature', series: results }];
         this.chartsPop = true;
         let lastElementToSelect = data.data.returnData[0].series.length - 1;
         this.currentTemperature = data.data.returnData[0].series[lastElementToSelect].value;
